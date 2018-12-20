2fb81f93f3c9324826bc896b81d3cc0a4bc2aa20
Angello Astorga <aastorg2@illinois.edu>
2018-12-20 14:13:37 -0600

    updating report parser to ignore some test resulting from internal pex failures and assuring all entries in feature vector are of type string

--- teacher/reportparser.py
+++ teacher/reportparser.py
@@ -21,18 +21,24 @@ def parseReport(reportPath):
 	tree = etree.parse(reportPath)
 	dataPoints = []
 	for test in tree.xpath('//generatedTest'):
+		
 		singlePoint = []
 		for value in test.xpath('./value'):
 			if re.match("^\$.*", value.xpath('./@name')[0]):
-				singlePoint.append(value.xpath('string()'))
+				singlePoint.append(str(value.xpath('string()')))
 
 		if test.get('status') == 'normaltermination':
 			singlePoint.append('true')
+
+		elif test.get('status') == 'assumptionviolation':
+			continue
+		elif test.get('status') == 'minimizationrequest':
+			continue
+		# REMIENDER: will need to add more cases for pex internal failures such as the above. We do not want to create feature from these values
 		else:
 			singlePoint.append('false')
 		# alternatives: test.get('failed') => true / None
 		# exceptionState
 		# failureText
-
 		dataPoints.append(singlePoint)
 	return dataPoints
