6c58856865ada8e730580e504811f5839ab248ac
Shambwaditya Saha <ssaha6@illinois.edu>
2019-03-31 16:17:47 -0500

    redundant option

--- seed.py
+++ seed.py
@@ -2,5 +2,4 @@ import subprocess
 subprocess.call(['setx', 'er_random_seed', '3', '/m'], shell=True)
 subprocess.call(['setx', 'pex_arithmetic_random_solver_attempts', '0', '/m'], shell=True) 
 subprocess.call(['setx', 'pex_arithmetic_random_solver_disabled', 'true', '/m'], shell=True) 
-subprocess.call(['setx', 'pex_arithmetic_avm_solver_max_restarts', '0', '/m'], shell=True) 
 
